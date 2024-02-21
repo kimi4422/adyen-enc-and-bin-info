00bfc639f2651134573f4d99b1c252910ef6153d
kimi4422 <157239026+kimi4422@users.noreply.github.com>
2024-02-21 19:30:15 +0800

    Update index.py

--- index.py
+++ index.py
@@ -79,7 +79,7 @@ async def bin(bin1):
 
 @app.get("/")
 async def start():
-    return RedirectResponse("http://www.github.com/r0ld3x/adyen-enc-and-bin-info")
+    return RedirectResponse("./bin/440393")
 
 
 class Item(BaseModel):
